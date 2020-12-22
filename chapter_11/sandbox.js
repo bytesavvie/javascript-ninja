f63c3d3de23eba0631fa80723eb74308924e87e9
bytesavvie <luckystan0525@gmail.com>
2020-12-22 11:54:34 +0100

    async and http requests

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,9 +1,13 @@
-console.log(1);
-console.log(2);
+// console.log(1);
+// console.log(2);
 
-setTimeout(() => {
-  console.log('callback func fired');
-}, 2000);
+// setTimeout(() => {
+//   console.log('callback func fired');
+// }, 2000);
 
-console.log(3);
-console.log(4);
+// console.log(3);
+// console.log(4);
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
