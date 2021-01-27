7cd732d986dff6b0511fb60703e0e4647d38f8d6
bytesavvie <luckystan0525@gmail.com>
2021-01-27 12:03:57 +0100

    ui-library and course finished!

--- ui-lib-project/src/index.js
+++ ui-lib-project/src/index.js
@@ -1,1 +1,28 @@
-console.log('index.js file');
+import Tooltip from './ninja-ui/tooltip.js';
+import Dropdown from './ninja-ui/dropdown.js';
+import Tabs from './ninja-ui/tabs.js';
+import Snackbar from './ninja-ui/snackbar.js';
+
+//create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('You Clicked Me :)');
+});
